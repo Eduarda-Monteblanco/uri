8eae04e6a966d5aafe84fdc8c9dbf0d026be8fb7
Eduarda <48218233+Eduarda-Monteblanco@users.noreply.github.com>
2020-02-23 21:04:34 -0300

    Add files via upload

--- 1021.py
+++ 1021.py
@@ -1,11 +1,62 @@
-'''
-Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário. A seguir, calcule o menor número de notas e moedas possíveis no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. A seguir mostre a relação de notas necessárias.
+valor = float(input())
+cem = valor // 100
+valor = valor - cem*100
 
-Entrada
-O arquivo de entrada contém um valor de ponto flutuante N (0 ≤ N ≤ 1000000.00).
+cinquenta = valor // 50
+valor = valor - cinquenta*50
 
-Saída
-Imprima a quantidade mínima de notas e moedas necessárias para trocar o valor inicial, conforme exemplo fornecido.
+vinte = valor // 20
+valor = valor - vinte*20
 
-Obs: Utilize ponto (.) para separar a parte decimal.
-'''
+dez = valor // 10
+valor = valor - dez*10
+
+cinco = valor // 5
+valor = valor - cinco*5
+
+dois = valor // 2
+valor = valor - dois*2
+
+um = valor // 1
+valor = valor - um*1
+um = float('%.2f'% um)
+valor = float('%.2f'% valor)
+
+cinquenta_cent = valor // 0.5
+valor = valor - cinquenta_cent*0.5
+cinquenta_cent = float('%.2f'% cinquenta_cent)
+valor = float('%.2f'% valor)
+
+vinte_e_cinco_cent = valor // 0.25
+valor = valor - vinte_e_cinco_cent*0.25
+vinte_e_cinco_cent = float('%.2f'% vinte_e_cinco_cent)
+valor = float('%.2f'% valor)
+
+dez_cent = valor // 0.10
+valor = valor - dez_cent*0.10
+dez_cent = float('%.2f'% dez_cent)
+valor = float('%.2f'% valor)
+
+cinco_cent = valor // 0.05
+valor = valor - cinco_cent*0.05
+cinco_cent = float('%.2f'% cinco_cent)
+valor = float('%.2f'% valor)
+
+um_cent = valor * 100
+um_cent = float('%.2f'% um_cent)
+valor = float('%.2f'% valor)
+
+print('NOTAS:')
+print('{} nota(s) de R$ 100.00'.format(int(cem)))
+print('{} nota(s) de R$ 50.00'.format(int(cinquenta)))
+print('{} nota(s) de R$ 20.00'.format(int(vinte)))
+print('{} nota(s) de R$ 10.00'.format(int(dez)))
+print('{} nota(s) de R$ 5.00'.format(int(cinco)))
+print('{} nota(s) de R$ 2.00'.format(int(dois)))
+print('MOEDAS:')
+print('{} moeda(s) de R$ 1.00'.format(int(um)))
+print('{} moeda(s) de R$ 0.50'.format(int(cinquenta_cent)))
+print('{} moeda(s) de R$ 0.25'.format(int(vinte_e_cinco_cent)))
+print('{} moeda(s) de R$ 0.10'.format(int(dez_cent)))
+print('{} moeda(s) de R$ 0.05'.format(int(cinco_cent)))
+print('{} moeda(s) de R$ 0.01'.format(int(um_cent)))
