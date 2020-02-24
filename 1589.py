f7702a2621d79bd314ee067a0170f35d9d1ef874
Eduarda-Monteblanco <eduardamonteblanco@gmail.com>
2020-02-24 14:40:09 -0300

    Removed conflicts

--- 1589.py
+++ 1589.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
 
@@ -16,23 +15,4 @@ t = int(input())
 for i in range(t):
     r1,r2 = map(int, input().split())
     raio = (r1*2 + r2*2)/2
-=======
-'''
-Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
-
-Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois círculos, qual o raio do menor círculo que possa englobar ambos os dois?
-
-Entrada
-Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
-
-Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
-Saída
-Em cada caso, imprima o menor raio possível em uma única linha
-'''
-
-t = int(input())
-for i in range(t):
-    r1,r2 = map(int, input().split())
-    raio = (r1*2 + r2*2)/2
->>>>>>> a
     print(int(raio))
